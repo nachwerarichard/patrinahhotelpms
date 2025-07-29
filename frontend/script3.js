58c85f90014ff789567734f49c3ea88efc4dcb0e
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2025-07-29 13:13:01 +0300

    Update script3.js

--- frontend/script3.js
+++ frontend/script3.js
@@ -850,12 +850,12 @@ bookingForm.addEventListener('submit', async function(event) {
     const nationality = document.getElementById('nationality').value;
     const address = document.getElementById('address').value;
     const phoneNo = document.getElementById('phoneNo').value;
-    const email = document.getElementById('guestEmail').value;
+    const guestEmail = document.getElementById('guestEmail').value;
     const nationalIdNo = document.getElementById('nationalIdNo').value;
     const bookingData = {
         name, room: roomNumber, checkIn, checkOut, nights, amtPerNight,
         totalDue, amountPaid, balance, paymentStatus, people, nationality,
-        address, phoneNo, email, nationalIdNo,
+        address, phoneNo, guestEmail, nationalIdNo,
         username: currentUsername // Pass username for audit log
     };
 
