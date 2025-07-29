f08724efdc85cad550b386b290784bc15642cd73
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2025-07-29 13:27:45 +0300

    Updated by adding guest email in editbooking () in  script3.js

--- frontend/script3.js
+++ frontend/script3.js
@@ -927,7 +927,7 @@ async function editBooking(id) {
         document.getElementById('nationality').value = booking.nationality;
         document.getElementById('address').value = booking.address;
         document.getElementById('phoneNo').value = booking.phoneNo;
-        document.getElementById('guestEmail').value = booking.email;
+        document.getElementById('guestEmail').value = booking.guestEmail;
         document.getElementById('nationalIdNo').value = booking.nationalIdNo;
         bookingModal.style.display = 'flex';
     } catch (error) {
