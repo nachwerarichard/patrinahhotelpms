b6a4bbcb2ab04d0693019c25d550afa70017d3fa
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2025-08-18 14:13:11 +0300

    Update script4.js

--- frontend/script4.js
+++ frontend/script4.js
@@ -413,7 +413,7 @@ loginForm.addEventListener('submit', async function(event) {
              //   method: 'POST',
                // headers: { 'Content-Type': 'application/json' },
                // body: JSON.stringify({ action: 'User Logged Out', user: currentUsername })
-            });//
+           // });//
       //  } catch (error) {
            / console.error('Error logging out action:', error);
             // Don't block logout if audit log fails
