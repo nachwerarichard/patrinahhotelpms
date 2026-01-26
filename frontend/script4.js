310f4251312260ce71e01068099ac95ac72295f1
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2026-01-26 21:28:21 +0300

    Update script4.js

--- frontend/script4.js
+++ frontend/script4.js
@@ -545,7 +545,6 @@ function applyRoleAccess(role) {
     document.getElementById('nav-booking').style.display = 'none';
     document.getElementById('nav-housekeeping').style.display = 'none';
     document.getElementById('nav-reports').style.display = 'none';
-        document.getElementById('overall').style.display = 'none';
 
     document.getElementById('nav-calendar').style.display = 'none';
     document.getElementById('nav-audit-logs').style.display = 'none';
@@ -556,7 +555,6 @@ function applyRoleAccess(role) {
             // Admins see everything
             document.getElementById('nav-booking').style.display = 'list-item';
             document.getElementById('nav-housekeeping').style.display = 'list-item';
-            document.getElementById('overall').style.display = 'list-item';
 
             document.getElementById('nav-reports').style.display = 'list-item';
             document.getElementById('nav-calendar').style.display = 'list-item';
