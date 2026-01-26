330252ffb82e0455ef6078f9f03c408db0ca9741
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2026-01-27 02:30:57 +0300

    Update script4.js

--- frontend/script4.js
+++ frontend/script4.js
@@ -479,7 +479,7 @@ event.preventDefault();
         
         // Fallback to a default accessible section if targetId is invalid
         if (currentUserRole === 'admin') {
-            document.getElementById('dashbaord').classList.add('active');
+            document.getElementById('booking-management').classList.add('active');
             //renderBookings(currentPage, currentSearchTerm); // Ensure it renders if fallback
         } else if (currentUserRole === 'housekeeper') {
             document.getElementById('housekeeping').classList.add('active');
