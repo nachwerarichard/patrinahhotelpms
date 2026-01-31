83e923f44b46e7d36ac59266fc9db4229990f65d
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2026-01-31 16:39:15 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -135,10 +135,9 @@ const bookingSchema = new mongoose.Schema({
     balance: { type: Number, default: 0 }, // This is ROOM balance
     paymentStatus: { type: String, enum: ['Pending', 'Paid', 'Partially Paid'], default: 'Pending' },
     paymentMethod: { type: String, enum: ['Cash', 'MTN Momo', 'Airtel Pay','Bank'], default: 'Cash' },
-    guestsource: { type: String, required: true, enum: ['walk in', 'OTA', 'Web'], default: 'walk in' },
+    guestsource: { type: String, required: true, enum: ['Walk in', 'Booking.com','Airbnd','Trip','Hotel Website', 'Expedia','Web'], default: 'walk in' },
     gueststatus: { type: String, required: true, enum: ['confirmed', 'cancelled', 'no show', 'checkedin', 'reserved','checkedout'], default: 'confirmed' },
     cancellationReason: { type: String, default: '' },
-
     people: { type: Number, required: true },
     transactionid: { type: String },
     extraperson:{ type: String },
