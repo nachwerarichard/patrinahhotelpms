7ef9086abbdd39429f9f48fb314c792f8c3ecb28
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2026-02-21 20:02:53 +0300

    Update script4.js

--- frontend/script4.js
+++ frontend/script4.js
@@ -5820,7 +5820,7 @@ document.addEventListener('DOMContentLoaded', () => {
 //const API_BASE_URL = 'https://novouscloudpms-tz4s.onrender.com';
  
 let authToken = localStorage.getItem('authToken') || ''; // <-- Issue is here
-let currentUsername = localStorage.getItem('username') || ''; 
+//let currentUsername = localStorage.getItem('username') || ''; 
 let currentUserRole = localStorage.getItem('userRole') || ''; 
 // ...
 
