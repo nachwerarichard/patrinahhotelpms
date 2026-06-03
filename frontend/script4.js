b6c5afe20e4d2075a0b9dcb291d21a144d0cc54a
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2026-06-03 08:06:33 +0000

    worked on the front office error

--- frontend/script4.js
+++ frontend/script4.js
@@ -740,7 +740,7 @@ async function showDashboard(username, role) {
         initialNavLinkId = 'nav-sales';
         document.getElementById('dashboard').style.display = 'none';
     }
-    else if (role === 'frontoffice') {
+    else if (role === 'Front office') {
         initialSectionId = 'booking-management';
         initialNavLinkId = 'nav-booking';
         document.getElementById('dashboard').style.display = 'none';
